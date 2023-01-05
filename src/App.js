9da4fb801e3665b0b836591d05b3eeb9713c4feb
SmgcGithub07 <saulo0707@hotmail.com>
2023-01-05 17:32:08 -0600

    Ya funcionan los links y el navegación. Iniciando el formulario de insertar enfermedades

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Listenf from "./componentes/catalogos/enfermedades/Listar-enfermedades";
+import Insert from "./componentes/catalogos/enfermedades/Insertar-enfermedades";
+import Editenf from "./componentes/catalogos/enfermedades/Editar-enfermedades";
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (     
+         <div className="container">            
+              <nav className="navbar navbar-expand navbar-light bg-light">
+                  <div className="nav navbar-nav">
+                      <a className="nav-item nav-link active" href="/">Sistema</a>
+                      <a className="nav-item nav-link" href="/">Inicio</a>
+                      <a className="nav-item nav-link" href="/listarenf">Enfermedades</a>
+                  </div>
+              </nav>
+              <br></br>                  
+              <Routes>  
+                  <Route path="/listarenf" element={<Listenf />} />
+                  <Route path="/insertenf" element={<Insert />} />
+                  <Route path="/editenf" element={<Editenf />} />
+            </Routes> 
+          </div>   
+       );
 }
-
 export default App;
