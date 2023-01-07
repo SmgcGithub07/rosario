1640e9e784ac546725d2a74d9c427436a0631704
SmgcGithub07 <saulo0707@hotmail.com>
2023-01-07 13:16:04 -0600

    Guarda y valida con la API, pero, al guardar no me envía al listado.

--- src/componentes/catalogos/enfermedades/Insertar-enfermedades.js
+++ src/componentes/catalogos/enfermedades/Insertar-enfermedades.js
@@ -10,14 +10,60 @@ class Insertarenfermedades extends React.Component {
         txtdescripenf:""
      }
 
+     cambiodeValor=(e)=>{
+        const state=this.state;
+        state[e.target.name]=e.target.value;
+        this.setState({state});
+     }
+
     enviarDatos = (e) =>{
           e.preventDefault();
           console.log("Enviando datos");
-    }
+          const {txtnombreenf, txtdescripenf}=this.state;
+          console.log(txtnombreenf);
+          console.log(txtdescripenf);
+
+          try {
+
+                if(this.txtnombreenf==null){
+                     alert("¡No puede dejar nulo el nombre de la enfermedad!");
+                }
+                else{
+                    var datosEnviar={Nombre_enf:txtnombreenf, Descrip:txtdescripenf}
+
+                    fetch("http://localhost/API/Catalogos/Enfermedades/Sp_Insert_Enfermedades.php",{
+                           method:"POST",
+                           body:JSON.stringify(datosEnviar)
+                    })
+                    .then(respuesta=>respuesta.json())
+                    .then((datosRespuesta)=>{                           
+                          switch (datosRespuesta) {
+                            case "No insertado, porque están nulos los datos":
+                                alert(datosRespuesta);
+                            break;
+
+                            case "No insertado, porque ya existe":
+                                alert(datosRespuesta);
+                            break;                         
+                            
+                            case "Registro guardado":
+                                alert(datosRespuesta);
+                                this.props.history.push("/listarenf");
+                            break;
+                          }
+                     }
+                    )
+                    .catch(console.log)
+                }    
+          } catch (error) {
+            console.log(error);
+          }
+    }   
 
     render() { 
 
         const {txtnombreenf, txtdescripenf}=this.state;
+         
 
         return ( 
              <div className="card">
@@ -25,28 +71,27 @@ class Insertarenfermedades extends React.Component {
                     Agregar Enfermedades
                 </div>
                 <div className="card-body">
-                    <form onSubmit={this.enviarDatos}>
-                        <div className="form-group">
-                            <table>     
-                                <tr>            
-                                  <td>Nombre de la enfermedad:</td>
-                                  <td colSpan="3"><input type="text" value={txtnombreenf} name="txtnombreenf" id="txtnombreenf" className="form-control" placeholder="Escribe el nombre de la enfermedad" aria-describedby="helpId" size={50} ></input></td>
-                               </tr> 
-                                <tr>            
-                                   <td>Descripción:</td>
-                                   <td colSpan="3"><input type="text" value={txtdescripenf} name="txtdescripenf" id="txtdescripenf" className="form-control" placeholder="Escribe la descripción de la enfermedad" aria-describedby="helpId" size={65} ></input></td>
-                                </tr>   
-                                <tr>
-                                 <div className="input-group">
-                                    <span className="input-group-btn">
+                    <form onSubmit={this.enviarDatos}>                      
+                                <div className="form-group">
+                                     <label htmlFor="">Nombre de la enfermedad:</label>
+                                     <input type="text" onChange={this.cambiodeValor} value={txtnombreenf}  name="txtnombreenf" id="txtnombreenf" className="form-control" aria-describedby="helpId" size={50}  placeholder="Teclea el nombre de la enfermedad" ></input>
+                                     
+                                </div>
+                                 
+                                <div className="form-group">
+                                     <label htmlFor="">Descripción de la enfermedad:</label>
+                                     <input type="text" onChange={this.cambiodeValor} value={txtdescripenf} name="txtdescripenf" id="txtdescripenf" className="form-control" aria-describedby="helpId" size={50}  placeholder="Teclea Descripción de la enfermedad" ></input>
+                                     
+                                </div>
+                          
+                                <div className="input-group">
+                                    <span className="input-group-btn">                                        
                                         <button className="btn btn-success" type="submit" aria-label="">Guardar</button>
                                         <Link to={"/listarenf"} className="btn btn-danger" type="button" aria-label="">Cancelar</Link>
                                    </span>                                    
                                  </div> 
-                                </tr>                                
-                            </table>        
-                        </div>                       
-                    </form>                              
+                              
+                    </form>                             
                 </div>
                 
              </div>
